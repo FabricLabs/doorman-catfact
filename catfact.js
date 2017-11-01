d491eb39fe86b89a64a89e59f9d80b0698076f03
Nate Richardson <hello@naterichardson.com>
2017-11-01 15:05:46 -0400

    fixed.

--- catfact.js
+++ catfact.js
@@ -2,32 +2,37 @@ const request = require('request');
 
 module.exports = Doorman => {
   return {
-    description: 'Gives a Random Cat Fact',
-    process: (msg, suffix, isEdit, cb) => {
-      request('https://catfact.ninja/fact',
-      function (err, res, body) {
-        try {
-        if (err) throw err;
-        var data = JSON.parse(body);
-        if (data && data.fact) {
-          cb({
-            embed: {
-              color: Doorman.Config.discord.defaultEmbedColor,
-              title: 'Cat Fact',
-              description: data.fact
+    commands: [
+      'catfact'
+    ],
+    catfact: {
+      description: 'Gives a Random Cat Fact',
+      process: (msg, suffix, isEdit, cb) => {
+        request('https://catfact.ninja/fact',
+        function (err, res, body) {
+          try {
+            if (err) throw err;
+            var data = JSON.parse(body);
+            if (data && data.fact) {
+              cb({
+                embed: {
+                  color: Doorman.Config.discord.defaultEmbedColor,
+                  title: 'Cat Fact',
+                  description: data.fact
+                }
+              }, msg);
             }
-          }, msg);
-        }
-        } catch (err) {
-          var msgTxt = `command cat_fact failed :disappointed_relieved:`;
-          if (Doorman.Config.debug) {
-            msgTxt += `\n${err.stack}`;
-            
-            Doorman.logError(err);
+          } catch (err) {
+            var msgTxt = `command cat_fact failed :disappointed_relieved:`;
+            if (Doorman.Config.debug) {
+              msgTxt += `\n${err.stack}`;
+              
+              Doorman.logError(err);
+            }
+            cb(msgTxt, msg);
           }
-          cb(msgTxt, msg);
-        }
-      });
+        });
+      }
     }
   };
 };
